13464f91acd2d6cce959329e39adc27aab18bcd3
Ryan Hartlage <ryanplusplus@gmail.com>
2021-01-03 18:39:15 -0500

    Updated stm8-tiny

--- src/main.c
+++ src/main.c
@@ -5,10 +5,9 @@
 
 #include <stdbool.h>
 #include <stddef.h>
-#include "stm8s.h"
+#include "interrupts.h"
 #include "clock.h"
 #include "tim4_system_tick.h"
-#include "pb5_heartbeat.h"
 #include "tiny_timer.h"
 #include "watchdog.h"
 
@@ -17,23 +16,22 @@ static tiny_timer_t timer;
 
 static void kick_watchdog(tiny_timer_group_t* timer_group, void* context)
 {
+  (void)timer_group;
   (void)context;
   watchdog_kick();
-  tiny_timer_start(timer_group, &timer, 1, kick_watchdog, NULL);
 }
 
 void main(void)
 {
-  disableInterrupts();
+  interrupts_disable();
   {
     watchdog_init();
     clock_init();
     tiny_timer_group_init(&timer_group, tim4_system_tick_init());
-    pb5_heartbeat_init(&timer_group);
   }
-  enableInterrupts();
+  interrupts_enable();
 
-  kick_watchdog(&timer_group, NULL);
+  tiny_timer_start_periodic(&timer_group, &timer, 10, kick_watchdog, NULL);
 
   while(true) {
     tiny_timer_group_run(&timer_group);
