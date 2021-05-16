7ebf13b42d89b1bfa41ab097332abf72bf3edd37
Ryan Hartlage <ryanplusplus@gmail.com>
2021-05-16 09:18:41 -0400

    Updated stm8-tiny

--- src/main.c
+++ src/main.c
@@ -13,27 +13,17 @@
 #include "watchdog.h"
 
 static tiny_timer_group_t timer_group;
-static tiny_timer_t timer;
-
-static void kick_watchdog(tiny_timer_group_t* timer_group, void* context)
-{
-  (void)timer_group;
-  (void)context;
-  watchdog_kick();
-}
 
 void main(void)
 {
   interrupts_disable();
   {
-    watchdog_init();
     clock_init();
     tiny_timer_group_init(&timer_group, tim4_system_tick_init());
+    watchdog_init(&timer_group);
     pb5_heartbeat_init(&timer_group);
   }
   interrupts_enable();
-
-  tiny_timer_start_periodic(&timer_group, &timer, 10, kick_watchdog, NULL);
 
   while(true) {
     tiny_timer_group_run(&timer_group);
